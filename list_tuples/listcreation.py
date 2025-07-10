f5389fafd5b9eac13bc96321395c389e4fe211b2
KenjiDOdate <kdodate@id.uff.br>
2025-07-09 21:25:35 -0300

    Creation of one exercise

---
+++ list_tuples/listcreation.py
@@ -0,0 +1,11 @@
+#Create a list of 5 names and display them all.
+
+namelist = []
+
+for i in range(0,5):
+    namelist.append(input("Insert a name: "))
+
+
+
+for i in range(0,6):
+    print(namelist[i])
